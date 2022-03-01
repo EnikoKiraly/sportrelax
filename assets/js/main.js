8d24b0eaed873923c00eebe2364450071d3f3336
EnikoKiraly <tomekrelski@gmail.com>
2022-03-01 12:28:11 +0100

    Copy to clipboard

--- assets/js/main.js
+++ assets/js/main.js
@@ -301,20 +301,14 @@ $(function() {
     
     
     //===== 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+       
 });
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+  alert("Skopiowane");
+}
